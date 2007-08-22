92ffd042253e54b81ca1e427b575cded2ebab6e8
Stéphane Gully <stephane.gully@gmail.com>
2007-08-22 12:49:02 +0000

    Changement d'orientation pour l'architecture des répertoires pear : lib/pear/bin/pear : pour le binaire lib/pear/php/ : pour les sources php des packages
    git-svn-id: file:///root/pkgi/trunk@21 1d9e17c5-d814-4e76-8053-328317d3193c

--- php/lib/pear/install-pear.sh
+++ php/lib/pear/install-pear.sh
@@ -7,7 +7,7 @@
 // - rajouter du code pour transformer ce script en script sh 
 //   de facon a permettre le reglage du cache_dir car il semble impossible de le regler en amont
 // - afficher le resultat
-// Remarque : Ce script étant un template, lorsqu''il sera instancié, le code php généré sera
+// Remarque : Ce script étant un tdemplate, lorsqu''il sera instancié, le code php généré sera
 //            en faite un script sh executable par l'utilisateur s''il desire installer pear
 //            dans son application
 //
@@ -24,13 +24,15 @@ $gopear = file_get_contents("http://pear.php.net/go-pear", false, $ct);
 
 // reglages des chemins
 $gopear = str_replace('detect_install_dirs();','detect_install_dirs();
-$prefix    = dirname(__FILE__);
-$temp_dir  = $prefix."/tmp/pear";
-$php_dir   = $prefix."/lib/pear";
-$cache_dir = $prefix."/tmp/pear/cache";
+$prefix    = "'.getenv('APPNAME_HOME').'/lib/pear";
+$temp_dir  = "'.getenv('APPNAME_HOME').'/tmp/pear";
+$php_dir   = "'.getenv('APPNAME_HOME').'/lib/pear/php";
+$cache_dir = "'.getenv('APPNAME_HOME').'/tmp/pear/cache";
+$test_dir  = "'.getenv('APPNAME_HOME').'/tmp/pear/tests";
+$doc_dir   = "'.getenv('APPNAME_HOME').'/tmp/pear/docs";
 ',$gopear);
 $gopear = str_replace('PEAR_Config::singleton()',
-                      'PEAR_Config::singleton(\''.getenv('APPNAME_HOME').'/etc/pear.conf'.'\')',
+                      'PEAR_Config::singleton(\''.getenv('APPNAME_HOME').'/lib/pear/pear.conf'.'\')',
                       $gopear);
 
 // repond automatiquement aux questions et retire les "press Enter to continue"
@@ -48,13 +50,13 @@ DIR=`dirname "$0"`
 cd "$DIR"
 $PHP -C -q -d output_buffering=1 "$FILE" $@
 mkdir -p '.getenv('APPNAME_HOME').'/tmp/pear/cache
-$PHP -r \'$f="'.getenv('APPNAME_HOME').'/bin/pear"; file_put_contents($f,str_replace("pearcmd.php \\"$@\\"","pearcmd.php -c '.getenv('APPNAME_HOME').'/etc/pear.conf \\"$@\\"",file_get_contents($f)));\'
-$PHP -r \'$f="'.getenv('APPNAME_HOME').'/bin/peardev"; file_put_contents($f,str_replace("pearcmd.php \\"$@\\"","pearcmd.php -c '.getenv('APPNAME_HOME').'/etc/pear.conf \\"$@\\"",file_get_contents($f)));\'
-$PHP -r \'$f="'.getenv('APPNAME_HOME').'/bin/pecl"; file_put_contents($f,str_replace("pearcmd.php \\"$@\\"","peclcmd.php -c '.getenv('APPNAME_HOME').'/etc/pear.conf \\"$@\\"",file_get_contents($f)));\'
-'.getenv('APPNAME_HOME').'/bin/pear config-set cache_dir '.getenv('APPNAME_HOME').'/tmp/pear/cache
-'.getenv('APPNAME_HOME').'/bin/pear config-set auto_discover 1
-'.getenv('APPNAME_HOME').'/bin/pear channel-discover pear.pxxo.net
-'.getenv('APPNAME_HOME').'/bin/pear config-show
+$PHP -r \'$f="'.getenv('APPNAME_HOME').'/lib/pear/bin/pear"; file_put_contents($f,str_replace("pearcmd.php \\"$@\\"","pearcmd.php -c '.getenv('APPNAME_HOME').'/lib/pear/pear.conf \\"$@\\"",file_get_contents($f)));\'
+$PHP -r \'$f="'.getenv('APPNAME_HOME').'/lib/pear/bin/peardev"; file_put_contents($f,str_replace("pearcmd.php \\"$@\\"","pearcmd.php -c '.getenv('APPNAME_HOME').'/lib/pear/pear.conf \\"$@\\"",file_get_contents($f)));\'
+$PHP -r \'$f="'.getenv('APPNAME_HOME').'/lib/pear/bin/pecl"; file_put_contents($f,str_replace("pearcmd.php \\"$@\\"","peclcmd.php -c '.getenv('APPNAME_HOME').'/lib/pear/pear.conf \\"$@\\"",file_get_contents($f)));\'
+'.getenv('APPNAME_HOME').'/lib/pear/bin/pear config-set cache_dir '.getenv('APPNAME_HOME').'/tmp/pear/cache
+'.getenv('APPNAME_HOME').'/lib/pear/bin/pear config-set auto_discover 1
+'.getenv('APPNAME_HOME').'/lib/pear/bin/pear channel-discover pear.pxxo.net
+'.getenv('APPNAME_HOME').'/lib/pear/bin/pear config-show
 exit
 <?php
 ob_end_clean();
