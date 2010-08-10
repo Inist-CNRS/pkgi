017c846d694250680c86ab57eeb86c00df990772
Stéphane Gully <stephane.gully@gmail.com>
2010-08-10 09:24:59 +0000

    Mise en place d'un tag "latest" permettant ainsi un téléchargement de la dernière version avec une URL unique.
    git-svn-id: file:///root/pkgi/trunk@330 1d9e17c5-d814-4e76-8053-328317d3193c

--- makerelease.sh
+++ makerelease.sh
@@ -11,16 +11,22 @@ if [ -w tags/$1 ] ; then
 fi
 
 # recuperation des tags
-svn co -q --depth=immediates https://subversion.cru.fr/pkgi/tags
+svn co --quiet --depth=immediates https://subversion.cru.fr/pkgi/tags
 
-# creation du tag
-svn cp -q https://subversion.cru.fr/pkgi/trunk tags/$1
+# creation du tag 
+svn cp --quiet https://subversion.cru.fr/pkgi/trunk tags/$1
 echo $1 > tags/$1/version
 svn rm tags/$1/makerelease.sh
 svn commit tags/$1 -q -m "Version $1"
 
+# création du lien symbolique vers la dernière version
+svn rm --quiet tags/latest
+svn commit tags/latest --quiet -m "Retire le lien vers la dernière version"
+svn cp --quiet tags/$1 tags/latest
+svn commit tags/latest --quiet -m "Mise à jour du lien vers la nouvelle version ($1)"
+
 # generation du tar.gz
-svn export -q tags/$1 pkgi
+svn export --quiet tags/$1 pkgi
 tar czf pkgi-$1.tar.gz pkgi/
 
 # divers nettoyages
