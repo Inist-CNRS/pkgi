c5ee423810530f33c7712490f2fdcf878ed43e3a
Stéphane Gully <stephane.gully@gmail.com>
2011-07-06 08:03:28 +0000

    Réglages pour empêcher de loguer les requêtes mysql dans un fichier car étrangement, si les log sont activées cela perturbe l'exécution de phpmyadmin. On désactive également l'affichage dans le navigateur des logs pour éviter les warnings non souhaités et surtout non corrigeable.
    git-svn-id: file:///root/pkgi/trunk@447 1d9e17c5-d814-4e76-8053-328317d3193c

--- phpmyadmin/etc/phpmyadmin/config.inc.php
+++ phpmyadmin/etc/phpmyadmin/config.inc.php
@@ -1,5 +1,13 @@
 <?php echo '<?php'; ?>
 
+// réglages pour empêcher de loguer les requêtes mysql dans un fichier
+// étrangement, si les log sont activées cela perturbe l'exécution de phpmyadmin
+// on désactive également l'affichage dans le navigateur des logs pour éviter
+// les warnings non souhaités et surtout non corrigeable
+ini_set('mysql.trace_mode', 'Off');
+ini_set('display_errors', 'Off');
+ini_set('display_startup_errors', 'Off');
+
 $i = 0;
 
 // chargement automatique des fichiers de conf modularisés
