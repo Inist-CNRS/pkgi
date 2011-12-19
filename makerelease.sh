3a9f080cfecd9aaf472e37679be8eb639d820a7d
Stéphane Gully <stephane.gully@gmail.com>
2011-12-19 11:41:12 +0100

    adapt build procedure: svn to git

--- makerelease.sh
+++ makerelease.sh
@@ -1,47 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! $# -eq 1 ] ; then
   echo "Usage $0 [Numero de version]"
   exit 1
 fi
+VERSION=$1
 
-if [ -w tags/$1 ] ; then
-  echo "Warning: version $1 already exists. Aborting ..."
+git pull --tags
+if [ "`git tag -l | grep $VERSION`" != 0 ] ; then
+  echo "Warning: version $VERISON already exists. Aborting ..."
   exit 2
 fi
 
-# divers nettoyages avant de commencer
-rm -rf /tmp/pkgi/
-rm -rf /tmp/tags/
-OLDDIR=`pwd`
-cd /tmp/
+git tag -d latest
+git push origin :latest
+git tag -a -m "Version $VERSION" latest
+git tag -a -m "Version $VERSION" $VERSION
+git push --tags
 
-# recuperation des tags
-echo "Récupération des tags"
-svn co --quiet --depth=immediates https://subversion.cru.fr/pkgi/tags
-
-# creation du tag 
-echo "Création du tag $1"
-svn cp --quiet https://subversion.cru.fr/pkgi/trunk tags/$1
-echo $1 > tags/$1/version
-svn rm --quiet tags/$1/makerelease.sh
-svn commit tags/$1 -q -m "Version $1"
-
-# création du lien symbolique vers la dernière version
-echo "Création du tag latest"
-svn rm --quiet tags/latest
-svn commit tags/latest --quiet -m "Retire le lien vers la dernière version"
-svn cp --quiet tags/$1 tags/latest
-svn commit tags/latest --quiet -m "Mise à jour du lien vers la nouvelle version ($1)"
-
-# generation du tar.gz
-echo "Génération de pkgi-$1.tar.gz"
-svn export --quiet tags/$1 pkgi
-tar czf $OLDDIR/pkgi-$1.tar.gz pkgi/
-
-# divers nettoyages
-echo "Nettoyages"
-rm -rf /tmp/pkgi/
-rm -rf /tmp/tags/
-
-echo "Release $1 générée"
+echo "Version $VERSION générée (disponible sur github)"
